a97bbb526b89f4be96d7757d8ba54641ae418854
P1T1J1 <lat0914@naver.com>
2017-07-21 22:26:17 +0900

    schema 파일 수정

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170721123356) do
+ActiveRecord::Schema.define(version: 20170721125336) do
+
+  create_table "answers", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "question_id"
+    t.string "answer"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.integer "lecture_id"
