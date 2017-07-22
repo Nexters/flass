b909f744a3a5b24541933cf62c03f2cc7661bbd7
yayshine <ys2657@columbia.edu>
2017-07-23 01:51:28 +0900

    VideoUpload 멍청한 컴포넌트에 URL로 동영상 업로드하는 부분 추가

--- client/components/Upload/VideoUpload.js
+++ client/components/Upload/VideoUpload.js
@@ -1,59 +1,89 @@
 import React, { Component } from 'react';
 import TextField from 'material-ui/TextField';
 import FlatButton from 'material-ui/FlatButton';
+import SelectField from 'material-ui/SelectField';
+import MenuItem  from 'material-ui/MenuItem';
 import PropTypes from 'prop-types';
 
 import './upload.scss';
 
+// constants
+const urlMethod = 0;
+const fileUploadMethod = 1;
+const noThumb = 0;
+const succThumb = 1;
+const failThumb = -1;
+
 const propTypes = {
-  handleNext: PropTypes.func
+  handleNext: PropTypes.func,
+  handleURLImport: PropTypes.func,
+  thumb: PropTypes.number,
+  thumbURL: PropTypes.string
 };
 const defaultProps = {
-  handleNext: () => { console.log('next'); }
+  handleNext: () => handleError('handleNext'),
+  handleURLImport: () => handleError('handleURLImport'),
+  thumb: noThumb,
+  thumbURL: ''
 };
+
+function handleError(func) {
+  console.error(`${func} is not defined`);
+}
 
 class VideoUpload extends Component {
   state = {
     title: '',
-    description: ''
+    description: '',
+    method: urlMethod,
+    url: ''
   };
 
   render() {
     const { handleNext } = this.props;
-    const { title, description } = this.state;
+    const { title, description, method } = this.state;
 
-    return(
+    return (
       <div>
         <div className="left-col">
-          <input
-            type="file"
-            onChange={this.handleUploadVideo}
-          />
+          <SelectField
+            floatingLabelText="업로드 방식"
+            value={ method }
+            onChange={ this.handleMethodChange }>
+            <MenuItem value={ urlMethod } primaryText="동영상 URL" />
+            <MenuItem value={ fileUploadMethod } primaryText="동영상 업로드" />
+          </SelectField>
+          {
+            method == urlMethod ?
+            this.renderURLField() : this.renderfileUploadField()
+          }
+          {
+            this.renderThumbnail()
+          }
         </div>
         <div className="right-col">
           <TextField
             floatingLabelText="제목"
             name="title"
-            value={title}
-            onChange={this.handleChange}
+            value={ title }
+            onChange={ this.handleChange }
             fullWidth
           />
           <TextField
             floatingLabelText="학습 목표"
             name="description"
-            value={description}
-            onChange={this.handleChange}
+            value={ description }
+            onChange={ this.handleChange }
             multiLine
             fullWidth
-            rows={15}
+            rows={ 15 }
           />
           <FlatButton
             onTouchTap={ () => handleNext(title, description) }
             className="next"
             backgroundColor="#7dcdf8"
-            hoverColor="#75a8da"
-          >
-            <span className="nextLabel">
+            hoverColor="#75a8da">
+            <span className="buttonLabel">
               다음
             </span>
           </FlatButton>
@@ -62,9 +92,69 @@ class VideoUpload extends Component {
     );
   }
 
-  handleChange = (e) => {
+  renderURLField = () => {
+    const { handleURLImport } = this.props;
+    const { url } = this.state;
+
+    return (
+      <div>
+        <TextField
+          floatingLabelText="URL"
+          name="url"
+          value={ url }
+          onChange={ this.handleChange }
+        />
+        <FlatButton
+          onTouchTap={ () => handleURLImport(url) }
+          backgroundColor="#7dcdf8"
+          hoverColor="#75a8da">
+          <span className="buttonLabel">
+            불러오기
+          </span>
+        </FlatButton>
+      </div>
+    );
+  }
+
+  renderfileUploadField = () => (
+    <div>
+      <FlatButton
+        backgroundColor="#7dcdf8"
+        hoverColor="#75a8da">
+        구글로 로그인
+      </FlatButton>
+    </div>
+  )
+
+  renderThumbnail = () => {
+    switch(this.props.thumb) {
+      case noThumb:
+        return;
+      case succThumb:
+        return (
+          <img
+            src={ this.props.thumbURL }
+            alt="succeeded importing video" />
+        );
+      case failThumb:
+      default:
+        return (
+          <img
+            src={ 'http://iamaperformer.com/userphotos/no-video-available.jpg' }
+            alt="failed at importing video" />
+        );
+    }
+  }
+
+  handleChange = e => {
     this.setState({
       [e.target.name]: e.target.value
+    });
+  }
+
+  handleMethodChange = (e, method) => {
+    this.setState({
+      method
     });
   }
 
