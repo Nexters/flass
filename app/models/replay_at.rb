61878022742890fccdda6907583bedd534257251
P1T1J1 <lat0914@naver.com>
2017-07-23 14:35:39 +0900

    모델 간 관계 설정

--- app/models/replay_at.rb
+++ app/models/replay_at.rb
@@ -1,2 +1,4 @@
 class ReplayAt < ApplicationRecord
+  belongs_to :user
+  belongs_to :lecture
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :replay_ats
+  has_many :lectures
+  has_many :answers
+  has_many :notifications
+  has_many :comments
 end
