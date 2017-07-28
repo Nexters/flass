915edaed5370fc4a7227a240f1cb6a62fccfbccc
P1T1J1 <lat0914@naver.com>
2017-07-28 15:10:18 +0900

    lecture와 question 컨트롤러의 index 액션 추가

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -4,7 +4,7 @@ class LecturesController < ApplicationController
   # GET /lectures
   # GET /lectures.json
   def index
-    @lectures = Lecture.find_by(user_id: session[:user_id])
+    @lectures = Lecture.where(user_id: session[:user_id])
   end
 
   # GET /lectures/1
