e10bb98007c9898246595fb0772ae764f796e2d9
P1T1J1 <lat0914@naver.com>
2017-07-29 23:59:48 +0900

    lecture, question 테이블 에러 메시지까지 추가

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,6 +1,5 @@
 class Lecture < ApplicationRecord
   belongs_to :user
 
-  validates_presence_of :title
-  validates_presence_of :content
+  validates_presence_of :title, :content, message: '제목과 내용을 입력해주세요!'
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -3,6 +3,5 @@ class Question < ApplicationRecord
   has_many :choices
   has_many :answers
 
-  validates_presence_of :content
-  validates_presence_of :correct_answer
+  validates_presence_of :content, :correct_answer, message: '질문과 정답을 입력해주세요!'
 end
