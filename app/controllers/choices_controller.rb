04e33d014c13cb5eaf9708cc512d74bb06f94bbf
Yoon Roh <shdbss111@gmail.com>
2017-07-30 18:37:59 +0900

    choice컨트롤러 create액션 수정

--- app/controllers/choices_controller.rb
+++ app/controllers/choices_controller.rb
@@ -7,59 +7,56 @@ class ChoicesController < ApplicationController
     @choices = Choice.all
   end
 
-  # GET /choices/1
-  # GET /choices/1.json
-  def show
-  end
+  # # GET /choices/1
+  # # GET /choices/1.json
+  # def show
+  # end
 
   # GET /choices/new
   def new
     @choice = Choice.new
   end
 
-  # GET /choices/1/edit
-  def edit
-  end
+  # # GET /choices/1/edit
+  # def edit
+  # end
 
   # POST /choices
   # POST /choices.json
   def create
     @choice = Choice.new(choice_params)
-
-    respond_to do |format|
+ 
       if @choice.save
-        format.html { redirect_to @choice, notice: 'Choice was successfully created.' }
-        format.json { render :show, status: :created, location: @choice }
+        render json: @choice, status: :created
       else
-        format.html { render :new }
-        format.json { render json: @choice.errors, status: :unprocessable_entity }
+        render json: @choice.errors, status: :unprocessable_entity
       end
     end
   end
 
-  # PATCH/PUT /choices/1
-  # PATCH/PUT /choices/1.json
-  def update
-    respond_to do |format|
-      if @choice.update(choice_params)
-        format.html { redirect_to @choice, notice: 'Choice was successfully updated.' }
-        format.json { render :show, status: :ok, location: @choice }
-      else
-        format.html { render :edit }
-        format.json { render json: @choice.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # # PATCH/PUT /choices/1
+  # # PATCH/PUT /choices/1.json
+  # def update
+  #   respond_to do |format|
+  #     if @choice.update(choice_params)
+  #       format.html { redirect_to @choice, notice: 'Choice was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @choice }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @choice.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
-  # DELETE /choices/1
-  # DELETE /choices/1.json
-  def destroy
-    @choice.destroy
-    respond_to do |format|
-      format.html { redirect_to choices_url, notice: 'Choice was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  # # DELETE /choices/1
+  # # DELETE /choices/1.json
+  # def destroy
+  #   @choice.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to choices_url, notice: 'Choice was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
