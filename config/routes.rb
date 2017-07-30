9d58add010b475aa615839e802132edc277f4a54
bobinlee <cultist_tp@naver.com>
2017-07-30 21:25:13 +0900

    빌드 깨짐 수정.

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,6 @@ Rails.application.routes.draw do
   resources :replay_ats
   resources :notifications
   resources :choices
-<<<<<<< HEAD
-  resources :lectures
-=======
->>>>>>> 9ee686b6d5b3e4c90a4e3b6814be9b3ce43fb0d4
   resources :answers
   resources :questions
   get 'home/index'
