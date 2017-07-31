ae3f79448ae07d41affbf94ea9d636b48971a2ba
Yoon Roh <shdbss111@gmail.com>
2017-07-31 16:12:31 +0900

    comment 컨트롤러 수정

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,7 @@ class CommentsController < ApplicationController
   # GET /comments
   # GET /comments.json
   def index
-    @comments = Comment.all
+    @comments = Comment.where(user_id: session[:user_id])
   end
 
   # GET /comments/1
