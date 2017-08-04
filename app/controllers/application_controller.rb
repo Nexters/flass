064a2fbbbfedaa45ec6f8ec445786c9f9a720839
P1T1J1 <lat0914@naver.com>
2017-08-05 01:40:57 +0900

    login check 구현중

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery prepend: true
   def login_check
-    if session[:user_id]=nil?
-      puts "로그인하세요"
+    begin
+      session[:user_id].nil?
+    rescue ActiveRecord::RecordNotFound => e
+      @error = {message: "Exception Message: #{e.message}", code: "401"}
+      render json: @error, status: :unauthorized
     end
   end
 end
