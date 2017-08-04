d5188cfe85791b58dbfe330da6a4e060be5c3d9b
P1T1J1 <lat0914@naver.com>
2017-08-04 14:33:38 +0900

    세션 구조 수정

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery prepend: true
   def login_check
-    if session[:user]=nil?
+    if session[:user_id]=nil?
       puts "로그인하세요"
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,10 @@ require 'net/http'
 require 'json'
 require 'openssl'
 
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+#OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 class UsersController < ApplicationController
+  before_action :login_check, only: [:show, :edit, :logout, :destroy]
+  before_action :set_user, only: [:show, :edit, :destroy]
   # GET /users
   # GET /users.json
   def show
@@ -29,30 +31,16 @@ class UsersController < ApplicationController
         @user.myprofileurl = about_user_list["picture"]
         #user생성되었을 경우와 그렇지 않은 경우 추가해야함
         if @user.save
-          session[:user] = @user
+          session[:user_id] = @user.id
           render json: @user, status: :created
         else
           #에러메시지
         end
       else
-        @user.username = about_user_list[:name]
-        @user.myprofileurl = about_user_list[:picture]
-        session[:user] = @user
+        @user.username = about_user_list["name"]
+        @user.myprofileurl = about_user_list["picture"]
+        session[:user_id] = @user.id
         render json: @user, status: :ok
-      end
-    end
-  end
-
-  # PATCH/PUT /users
-  # PATCH/PUT /users.json
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -65,16 +53,13 @@ class UsersController < ApplicationController
   # DELETE /users/1.json
   def destroy
     @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    format.json { head :no_content }
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+      @user = User.find(session[:user_id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
