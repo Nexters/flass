c684045aca14ad51e01766db0894927d89c525e6
luke9407 <luke9407@snu.ac.kr>
2017-08-04 14:46:38 +0900

    apipie base_url 수정

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -1,6 +1,6 @@
 Apipie.configure do |config|
   config.app_name                = "RailsReactBoilerplate"
-  config.api_base_url            = "/api"
+  config.api_base_url            = ""
   config.doc_base_url            = "/apipie"
   # where is your API defined?
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/**/*.rb"
