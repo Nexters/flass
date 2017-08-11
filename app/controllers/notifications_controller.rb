13efe1da8cf09cfa08c4e9d6029a042777f3b5cd
luke9407 <luke9407@snu.ac.kr>
2017-08-11 12:20:57 +0900

    notification, replay_at 에러 메시지 수정. 모델 validation 추가

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,4 +1,5 @@
 class NotificationsController < ApplicationController
+  before_action :login_check
   before_action :set_notification, only: [:update, :destroy]
 
   api :GET, '/notifications', '유저 알림'
@@ -8,6 +9,7 @@ class NotificationsController < ApplicationController
   end
 
   api :POST, '/notifications', '유저 알림 생성'
+  param :user_id, :number, :desc => "알림을 보낼 유저 ID", :required => true
   param :notification_type, String, :desc => "알림 타입", :required => true
   param :content, String, :desc => "알림 내용", :required => true
   param :url, String, :desc => "알림 url 정보", :required => true
@@ -17,7 +19,7 @@ class NotificationsController < ApplicationController
     if @notification.save
       render json: @notification, status: :ok
     else
-      render json: @notification.errors, status: :unprocessable_entity
+      render json: {message: "유저 ID, 알림 내용 등을 입력해주세요."}, status: :bad_request
     end
   end
 
@@ -30,15 +32,19 @@ class NotificationsController < ApplicationController
     if @notification.update(notification_params)
       render json: @notification, status: :ok
     else
-      render json: @notification.errors, status: :unprocessable_entity
+      render json: {message: "유저 ID, 알림 내용 등을 입력해주세요."}, status: :bad_request
     end
   end
 
   api :DELETE, '/notifications', '유저 알림 삭제'
   param :id, :number, :desc => "notification ID", :required => true
   def destroy
-    @notification.destroy
-    head :no_content
+    if @notification.user_id == session[:user_id]
+      @notification.destroy
+      head :ok
+    else
+      render json: {message: "알람을 삭제할 권한이 없습니다."}, status: :unauthorized
+    end
   end
 
   private
@@ -49,7 +55,6 @@ class NotificationsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def notification_params
-      params['user_id'] = session[:user_id]
       params.permit(:user_id, :notification_type, :content, :url)
     end
 end
