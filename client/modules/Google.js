69d88a5ef83ab37c0eaa8dc30bde620390964293
yayshine <ys2657@columbia.edu>
2017-08-12 15:15:53 +0900

    Add Google's MediaUploader

--- client/modules/Google.js
+++ client/modules/Google.js
@@ -44,7 +44,7 @@ export default class Google {
   }
 
   static requestThumbClient(youtubeVideoId) {
-    gapi.client.request({
+    return gapi.client.request({
       method: 'GET',
       path: '/youtube/v3/videos',
       params: {
@@ -57,5 +57,72 @@ export default class Google {
   static authenticate() {
     const auth = gapi.auth2.getAuthInstance();
     auth.signIn();
+  }
+
+  static uploadVideo(file, accessToken){
+    var metadata = {
+      snippet: {
+        title: "title",
+        description: "description",
+        tags: ["tag1", ["tag2"]]
+      },
+      status: {
+        privacyStatus: "unlisted"
+      }
+    };
+    let uploadStartTime;
+    var uploader = new MediaUploader({
+      baseUrl: 'https://www.googleapis.com/upload/youtube/v3/videos',
+      file: file,
+      token: accessToken,
+      metadata: metadata,
+      params: {
+        part: Object.keys(metadata).join(',')
+      },
+      onError: function(data) {
+        var message = data;
+        // Assuming the error is raised by the YouTube API, data will be
+        // a JSON string with error.message set. That may not be the
+        // only time onError will be raised, though.
+        try {
+          var errorResponse = JSON.parse(data);
+          message = errorResponse.error.message;
+        } finally {
+          alert(message);
+        }
+      }.bind(this),
+      onProgress: function(data) {
+        var currentTime = Date.now();
+        var bytesUploaded = data.loaded;
+        var totalBytes = data.total;
+        // The times are in millis, so we need to divide by 1000 to get seconds.
+        var bytesPerSecond = bytesUploaded / ((currentTime - uploadStartTime) / 1000);
+        var estimatedSecondsRemaining = (totalBytes - bytesUploaded) / bytesPerSecond;
+        var percentageComplete = (bytesUploaded * 100) / totalBytes;
+
+        // $('#upload-progress').attr({
+        //   value: bytesUploaded,
+        //   max: totalBytes
+        // });
+        //
+        // $('#percent-transferred').text(percentageComplete);
+        // $('#bytes-transferred').text(bytesUploaded);
+        // $('#total-bytes').text(totalBytes);
+        //
+        // $('.during-upload').show();
+        console.log(bytesUploaded+" / "+totalBytes);
+      }.bind(this),
+      onComplete: function(data) {
+        var uploadResponse = JSON.parse(data);
+        // this.videoId = uploadResponse.id;
+        // $('#video-id').text(this.videoId);
+        // $('.post-upload').show();
+        // this.pollForVideoStatus();
+        console.log(uploadResponse);
+      }.bind(this)
+    });
+    // This won't correspond to the *exact* start of the upload, but it should be close enough.
+    uploadStartTime = Date.now();
+    uploader.upload();
   }
 }
