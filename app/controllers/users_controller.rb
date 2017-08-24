9bf5128ed6b6d277f676e1822e6acec661396018
P1T1J1 <lat0914@naver.com>
2017-08-24 19:36:15 +0900

    user post 수정

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,7 @@ class UsersController < ApplicationController
     render json: @user
   end
 
+=begin
   # POST /users
   # POST /users.json
   api :POST, '/users', '회원 정보 생성 및 업데이트하기'
@@ -35,8 +36,8 @@ class UsersController < ApplicationController
       render json: @user, status: :ok
     end
   end
+=end
 
-=begin
   # POST /users
   # POST /users.json
   api :POST, '/users', '회원 정보 생성 및 업데이트하기'
@@ -71,7 +72,7 @@ class UsersController < ApplicationController
       end
     end
   end
-=end
+
 
 
   def login
