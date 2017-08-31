fece20888914233e5e975924e66c30c5f7928540
bobinlee <cultist_tp@naver.com>
2017-08-31 10:27:34 +0900

    redux dev tool 이용.

--- client/store.js
+++ client/store.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import reducer from './reducers';
 import rootSaga from './sagas';
 
@@ -10,9 +11,9 @@ export default function configureStore(initialState) {
   const store = createStore(
     reducer,
     initialState,
-    applyMiddleware(
+    composeWithDevTools(applyMiddleware(
       sagaMiddleware, thunkMiddleware, createLogger()
-    )
+    ))
   );
   sagaMiddleware.run(rootSaga);
   return store;
