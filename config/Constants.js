4538e7dcf7a036a71ad559f357ad532c0aaefd82
zerofruit <14wnrkim@gmail.com>
2017-09-17 18:56:28 +0900

    설정 변경

--- config/Constants.js
+++ config/Constants.js
@@ -1,2 +1,2 @@
-export const GOOGLE_API_KEY = 'AIzaSyD4aRRdCvbqMm9b3fXb_lVYEu_lrys3Ep4';
-export const GOOGLE_CLIENT_KEY = '686640487170-a75di4ght3uniafb99vofe2n63a66ual.apps.googleusercontent.com';
+export const GOOGLE_API_KEY = 'AIzaSyDGzhWnqW9tBpMGtMEqgo9rvGgj2ZHKLCI';
+export const GOOGLE_CLIENT_KEY = '94680954925-k6rvvoj4aui3od553lke3v3dtb8kcq3u.apps.googleusercontent.com';
