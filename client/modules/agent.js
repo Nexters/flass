16e197a6424121e5149545cd96643467098e58f3
bobinlee <cultist_tp@naver.com>
2017-09-24 17:02:11 +0900

    api 일 경우, 아닐 경우 분리.

--- client/modules/agent.js
+++ client/modules/agent.js
@@ -36,23 +36,32 @@ function selectAPIRequest(railsRequest = {}, jsonRequest = {}) {
   return TYPE_OF_BACKEND === 'json' ?  jsonRequest : railsRequest;
 }
 
+const requestsForApi = {
+  del: url =>
+    axios.delete(`${API_ROOT}/api${url}`, config).then(responseBody),
+  get: url =>
+    axios.get(`${API_ROOT}/api${url}`, config).then(responseBody),
+  put: (url, body) =>
+    axios.put(`${API_ROOT}/api${url}`, body, config).then(responseBody),
+  post: (url, body) =>
+    axios.post(`${API_ROOT}/api${url}`, body, config).then(responseBody)
+};
+
 const requests = {
   del: url =>
-    axios.delete(`${API_ROOT}/api/${url}`, config).then(responseBody),
+    axios.delete(`${API_ROOT}${url}`, config).then(responseBody),
   get: url =>
-    axios.get(`${API_ROOT}/api/${url}`, config).then(responseBody),
+    axios.get(`${API_ROOT}${url}`, config).then(responseBody),
   put: (url, body) =>
-    axios.put(`${API_ROOT}/api/${url}`, body, config).then(responseBody),
+    axios.put(`${API_ROOT}${url}`, body, config).then(responseBody),
   post: (url, body) =>
-    axios.post(`${API_ROOT}/api/${url}`, body, config).then(responseBody)
+    axios.post(`${API_ROOT}${url}`, body, config).then(responseBody)
 };
 
-const Auth = {
-
-};
+const Auth = {};
 
 const UserJson = {
-  me: () => requests.get('/json/FlassUser.json')
+  me: () => requestsForApi.get('/json/FlassUser.json')
         .then(response => {
           console.log('response::UserJson');
           console.log(response);
@@ -60,14 +69,14 @@ const UserJson = {
         })
 };
 const UserRails = {
-  me: token => requests.post('/users', { id_token: token })
+  me: token => requestsForApi.post('/users', { id_token: token })
     .then(response => {
       console.log('response::UserRails');
       console.log(response);
       return response;
     }),
-  byId: id => requests.get(`/users/${id}`),
-  whoami: () => requests.get('/users')
+  byId: id => requestsForApi.get(`/users/${id}`),
+  whoami: () => requestsForApi.get('/users')
     .then(response => {
       console.log('response::UserRails::whoami');
       console.log(response);
@@ -84,66 +93,66 @@ const User = selectAPIRequest(UserRails, UserJson);
 
 
 const Badge = {
-  byType: type => requests.get('/json/FlassBadgeHistory.json')
+  byType: type => requestsForApi.get('/json/FlassBadgeHistory.json')
 };
 
 const GridJson = {
-  all: () => requests.get('/json/FlassGrid.json')
+  all: () => requestsForApi.get('/json/FlassGrid.json')
 };
 const GridRails = {
-  all: () => requests.get('/lectures')
+  all: () => requestsForApi.get('/lectures')
 };
 const Grid = selectAPIRequest(GridRails, GridJson);
 
 
 const LectureJson = {
-  byId: lectureId => requests.get('/json/FlassLecture.json')
+  byId: lectureId => requestsForApi.get('/json/FlassLecture.json')
 };
 const LectureRails = {
-  byId: lectureId => requests.get(`/lectures/${lectureId}`),
-  upload: body => requests.post('/lectures', body)
+  byId: lectureId => requestsForApi.get(`/lectures/${lectureId}`),
+  upload: body => requestsForApi.post('/lectures', body)
 };
 const Lecture = selectAPIRequest(LectureRails, LectureJson);
 
 
 const QuestionJson = {
-  byLectureId: lectureId => requests.get('/json/FlassQuestion.json')
+  byLectureId: lectureId => requestsForApi.get('/json/FlassQuestion.json')
 };
 const QuestionRails = {
-  byLectureId: lectureId => requests.get(`/questions?lecture_id=${lectureId}`),
-  uploadByLectureId: body => requests.post('/questions', body)
+  byLectureId: lectureId => requestsForApi.get(`/questions?lecture_id=${lectureId}`),
+  uploadByLectureId: body => requestsForApi.post('/questions', body)
 };
 const Question = selectAPIRequest(QuestionRails, QuestionJson);
 
 const Choice = {
-  upload: body => requests.post('/choices', body),
-  fetch: questionId => requests.get(`/choices?question_id=${questionId}`)
+  upload: body => requestsForApi.post('/choices', body),
+  fetch: questionId => requestsForApi.get(`/choices?question_id=${questionId}`)
 };
 
 const CommentJson = {
-  byLectureId: lectureId => requests.get('/json/FlassComment.json'),
-  postComment: (lectureId, content) => requests.get('/json/FlassPostComment.json', { lectureId, content }),
-  deleteById: commentId => requests.del('')
+  byLectureId: lectureId => requestsForApi.get('/json/FlassComment.json'),
+  postComment: (lectureId, content) => requestsForApi.get('/json/FlassPostComment.json', { lectureId, content }),
+  deleteById: commentId => requestsForApi.del('')
 };
 
 const CommentRails = {
-  byLectureId: lectureId => requests.get(`/comments?lecture_id=${lectureId}`),
-  postComment: (lectureId, content) => requests.post('/comments', { lecture_id: lectureId, content }),
-  postReplyComment: (commentId, content) => requests.post('/comment_children', { comment_id: commentId, content }),
-  deleteById: commentId => requests.del(`/comments?id=${commentId}`),
-  deleteReplyById: id => requests.del('/comment_children', { id })
+  byLectureId: lectureId => requestsForApi.get(`/comments?lecture_id=${lectureId}`),
+  postComment: (lectureId, content) => requestsForApi.post('/comments', { lecture_id: lectureId, content }),
+  postReplyComment: (commentId, content) => requestsForApi.post('/comment_children', { comment_id: commentId, content }),
+  deleteById: commentId => requestsForApi.del(`/comments?id=${commentId}`),
+  deleteReplyById: id => requestsForApi.del('/comment_children', { id })
 };
 
 const Comment = selectAPIRequest(CommentRails, CommentJson);
 
 const Like = {
-  postByCommentId: (commentId) => requests.put(`/comments/${commentId}/like`),
+  postByCommentId: (commentId) => requestsForApi.put(`/comments/${commentId}/like`),
 };
 
 const AnswerRails = {
-  byLectureId: lectureId => requests.get(`/answers?lecture_id=${lectureId}`),
-  uploadByQuestionId: body => requests.post('/answers', body),
-  getAnswerByQuestionId: questionId => requests.get(`/answers/question?question_id=${questionId}`)
+  byLectureId: lectureId => requestsForApi.get(`/answers?lecture_id=${lectureId}`),
+  uploadByQuestionId: body => requestsForApi.post('/answers', body),
+  getAnswerByQuestionId: questionId => requestsForApi.get(`/answers/question?question_id=${questionId}`)
 };
 const AnswerJson = {
   byLectureId: () => {},
@@ -154,7 +163,7 @@ const Answer = selectAPIRequest(AnswerRails, AnswerJson);
 
 
 const Analysis = {
-  fetch: lectureId => requests.get(`/lectures/statistics?id=${lectureId}`)
+  fetch: lectureId => requestsForApi.get(`/lectures/statistics?id=${lectureId}`)
 };
 
 export default {
