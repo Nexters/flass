632c09bf970ff9137d70b38635161d21c5b841de
bobinlee <cultist_tp@naver.com>
2017-10-16 00:48:36 +0900

    에러 수정.

--- client/config/EnvironmentConfig.js
+++ client/config/EnvironmentConfig.js
@@ -3,8 +3,7 @@ const API_JSON = 'http://localhost:4000';
 const API_LOCAL = 'http://localhost:3000';
 const API_PRODUCTION = 'http://flass.me';
 
-export const isDevelop = TYPE_OF_BACKEND == 'local';
-
+export const isDevelop = () => TYPE_OF_BACKEND == 'local';
 
 export const API_ROOT = (function(type) {
   switch(type) {
